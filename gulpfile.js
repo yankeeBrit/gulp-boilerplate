ee456f3d42a0d2394224d5dd0b061e2e0c658955
Nikolai Webb <nikolaiwebb@Nikolais-MacBook-Pro.local>
2017-05-26 13:05:53 -0400

    General update.

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,39 @@
-/* File: gulpfile.js */
+'use strict';
 
-// grab our packages
 var gulp = require('gulp'),
     gutil = require('gulp-util'),
-    jshint = require('gulp-jshint'),
     sass = require('gulp-sass'),
     sourcemaps = require('gulp-sourcemaps'),
+    bourbon = require('node-bourbon').includePaths,
+    neat = require('node-neat').includePaths,
     autoprefixer = require('gulp-autoprefixer'),
+    cleanCSS = require('gulp-clean-css'),
+    jshint = require('gulp-jshint'),
     concat = require('gulp-concat'),
+    uglify = require('gulp-uglify'),
     browserSync = require('browser-sync');
+		
 
-// define the default task and add the watch task to it
+// Live reload anytime a file changes
 gulp.task('default', ['watch']);
 
-// configure the build-css task
+// configure which files to watch and what tasks to use on file changes
+gulp.task('watch', ['browserSync', 'build-css', 'jshint'], function() {
+  gulp.watch('src/scss/**/*.scss', ['build-css']);
+  gulp.watch('src/js/**/*.js', ['jshint']);
+  gulp.watch('dist/*.html').on('change', browserSync.reload);
+});
+
+// Compile SASS files
 gulp.task('build-css', function() {
   return gulp.src('src/scss/**/*.scss')
     .pipe(sourcemaps.init())  // Process the original sources
-    .pipe(sass())
+    .pipe(sass({
+			  includePaths: bourbon,
+			  includePaths: neat
+      }))
     .pipe(autoprefixer())
+    .pipe(cleanCSS())
     .pipe(sourcemaps.write()) // Add the map to modified source.
     .pipe(gulp.dest('dist/css'));
 });
@@ -35,8 +50,7 @@ gulp.task('build-js', function() {
   return gulp.src('src/js/**/*.js')
     .pipe(sourcemaps.init())
     .pipe(concat('bundle.js'))
-    //only uglify if gulp is ran with '--type production'
-    .pipe(gutil.env.type === 'production' ? uglify() : gutil.noop()) 
+    .pipe(uglify())
     .pipe(sourcemaps.write())
     .pipe(gulp.dest('dist/js'));
 });
@@ -48,11 +62,4 @@ gulp.task('browserSync', function() {
       baseDir: 'dist'
     }
   })
-});
-
-// configure which files to watch and what tasks to use on file changes
-gulp.task('watch', ['browserSync', 'build-css', 'jshint'], function() {
-  gulp.watch('src/scss/**/*.scss', ['build-css']);
-  gulp.watch('src/js/**/*.js', ['jshint']);
-  gulp.watch('dist/*.html').on('change', browserSync.reload);
 });
